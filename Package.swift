95629c58fbd06fd84d2fb9d744326f723acaa3b9
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2025-07-20 23:13:53 +1000

    Add macro target

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "Exhaust",
@@ -17,9 +18,14 @@ let package = Package(
             name: "Exhaust",
             targets: ["Exhaust"]
         ),
+        .library(
+            name: "ExhaustMacros",
+            targets: ["ExhaustMacros"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-syntax", from: "600.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -28,6 +34,15 @@ let package = Package(
             name: "Exhaust",
             dependencies: [
                 .product(name: "CasePaths", package: "swift-case-paths"),
+                "ExhaustMacros",
+            ]
+        ),
+        .macro(
+            name: "ExhaustMacros",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
             ]
         ),
         .testTarget(
