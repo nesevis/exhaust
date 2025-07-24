950abeb6a492cc2a0ec5ecdc9ca409914a93c4e2
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2025-07-25 06:12:25 +1000

    Add generator validation convenience

---
+++ Tests/ExhaustTests/Fixtures/ReflectiveGenerator.swift
@@ -0,0 +1,19 @@
+//
+//  ReflectiveGenerator.swift
+//  Exhaust
+//
+//  Created by Chris Kolbu on 25/7/2025.
+//
+
+@testable import Exhaust
+import Testing
+
+@discardableResult
+func validateGenerator<Output: Equatable>(_ gen: ReflectiveGenerator<Any, Output>) throws -> (recipe: ChoiceTree, instance: Output) {
+    let instance = try #require(Interpreters.generate(gen))
+    let recipe = try #require(try Interpreters.reflect(gen, with: instance))
+    let replay = try #require(Interpreters.replay(gen, using: recipe))
+    print()
+    #expect(instance == replay)
+    return (recipe, instance)
+}
