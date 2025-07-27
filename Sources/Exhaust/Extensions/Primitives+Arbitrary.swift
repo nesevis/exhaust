5ce5336b6adff410425718deef2c4a3ce5f6a035
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2025-07-28 09:41:03 +1000

    Clean up Arbitrary conformances

--- Sources/Exhaust/Extensions/Primitives+Arbitrary.swift
+++ Sources/Exhaust/Extensions/Primitives+Arbitrary.swift
@@ -5,6 +5,74 @@
 //  Created by Chris Kolbu on 18/7/2025.
 //
 
+// MARK: - Unsigned Integers
+
+extension UInt64: Arbitrary {
+    static var arbitrary: ReflectiveGenerator<Any, Self> {
+        Gen.getSize().bind { size in
+            let expanded: UInt64 = size < 64 ? 1 << size : UInt64.max
+            return Gen.choose(in: UInt64.min...expanded)
+        }
+    }
+}
+
+extension UnsignedInteger {
+    static var arbitrary: ReflectiveGenerator<Any, Self> {
+        UInt64.arbitrary
+            .mapped(forward: { Self(truncatingIfNeeded: $0) }, backward: { UInt64($0) })
+    }
+}
+
+extension UInt: Arbitrary {}
+extension UInt8: Arbitrary {}
+extension UInt16: Arbitrary {}
+extension UInt32: Arbitrary {}
+
+// MARK: - Signed integers
+
+extension Int64: Arbitrary {
+    static var arbitrary: ReflectiveGenerator<Any, Self> {
+        Gen.getSize().bind { size in
+            let expanded: UInt64 = size < 63 ? 1 << size : UInt64(Int64.max)
+            return Gen.choose(in: -Int64(expanded)...Int64(expanded))
+        }
+    }
+}
+
+extension SignedInteger {
+    static var arbitrary: ReflectiveGenerator<Any, Self> {
+        Int64.arbitrary
+            .mapped(forward: { Self(truncatingIfNeeded: $0) }, backward: { Int64($0) })
+    }
+}
+
+extension Int: Arbitrary {}
+extension Int8: Arbitrary {}
+extension Int16: Arbitrary {}
+extension Int32: Arbitrary {}
+
+// MARK: - Floating points
+
+extension Double: Arbitrary {
+    static var arbitrary: ReflectiveGenerator<Any, Self> {
+        Gen.getSize().bind { size in
+            // TODO: use pow() to scale range   
+            Gen.choose(in: -Double(size)...Double(size))
+        }
+    }
+}
+
+extension BinaryFloatingPoint {
+    static var arbitrary: ReflectiveGenerator<Any, Self> {
+        Double.arbitrary
+            .mapped(forward: { Self($0) }, backward: { Double($0) })
+    }
+}
+
+extension Float: Arbitrary {}
+
+// MARK: - Boolean
+
 extension Bool: Arbitrary {
     static var arbitrary: ReflectiveGenerator<Any, Bool> {
         Gen.pick(choices: [
@@ -14,109 +82,16 @@ extension Bool: Arbitrary {
     }
 }
 
-extension UInt8: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, UInt8> {
-        Gen.choose(in: UInt8.min...UInt8.max)
-    }
-}
-
-extension UInt16: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, UInt16> {
-        Gen.choose(in: UInt16.min...UInt16.max)
-    }
-}
-
-extension UInt32: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, UInt32> {
-        Gen.choose(in: UInt32.min...UInt32.max)
-    }
-}
-
-extension UInt64: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, UInt64> {
-        Gen.getSize().bind { size in
-            Gen.choose(in: UInt64.min...size)
-        }
-    }
-}
-
-extension UInt: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, UInt> {
-        Gen.choose(in: UInt.min...UInt.max)
-    }
-}
-
-extension Int8: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Int8> {
-        Gen.choose(in: Int8.min...Int8.max)
-    }
-}
-
-extension Int16: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Int16> {
-        Gen.choose(in: Int16.min...Int16.max)
-    }
-}
-
-extension Int32: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Int32> {
-        Gen.choose(in: Int32.min...Int32.max)
-    }
-}
-
-extension Int64: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Int64> {
-        Gen.choose(in: Int64.min...Int64.max)
-    }
-}
-
-extension Int: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Self> {
-        Gen.getSize().bind { size in
-            Gen.choose(in: -Int(size)...Int(size))
-        }
-    }
-}
-
-extension Float: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Float> {
-        Gen.choose(in: -Float.greatestFiniteMagnitude...Float.greatestFiniteMagnitude)
-    }
-}
-
-extension Double: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Double> {
-        Gen.getSize().bind { size in
-            Gen.choose(in: -Double(size)...Double(size))
-        }
-//        Gen.choose(in: -Double.greatestFiniteMagnitude...Double.greatestFiniteMagnitude)
-    }
-}
-
-extension Unicode.Scalar: Arbitrary {
-    static var arbitrary: ReflectiveGenerator<Any, Unicode.Scalar> {
-        Gen.getSize().bind { size in
-            Gen.pick(choices: [
-                (200, Gen.choose(in: 32...126)), // Standard ascii
-                (size, Gen.choose(in: self.bitPatternRanges[0])),
-                ((size + 2) / 2, Gen.choose(in: self.bitPatternRanges[1]))
-            ])
-        }
-        .mapped(
-            forward: { Unicode.Scalar(UInt32($0))! },
-            backward: { UInt64($0) }
-        )
-    }
-}
+// MARK: - Strings and Characters
 
 extension Character: Arbitrary {
     static var arbitrary: ReflectiveGenerator<Any, Character> {
         Gen.getSize().bind { size in
             Gen.pick(choices: [
                 (200, Gen.chooseCharacter(in: self.bitPatternRanges[0])), // Standard ascii
-                (size, Gen.chooseCharacter(in: self.bitPatternRanges[1])), // Null bytes, tab characters
-                (size, Gen.chooseCharacter(in: self.bitPatternRanges[2])),
-                ((size + 2) / 2, Gen.chooseCharacter(in: self.bitPatternRanges[3]))
+                (size, Gen.chooseCharacter(in: self.bitPatternRanges[1])), // Control characters
+                (size, Gen.chooseCharacter(in: self.bitPatternRanges[2])), // First bit of unicode minus ascii
+                ((size + 2) / 2, Gen.chooseCharacter(in: self.bitPatternRanges[3])) // Second bit
             ])
         }
     }
@@ -128,6 +103,8 @@ extension String: Arbitrary {
             .map { String($0) }
     }
 }
+
+// MARK: - Optional
 
 extension Optional: Arbitrary where Wrapped: Arbitrary, Wrapped: Equatable {
     static var arbitrary: ReflectiveGenerator<Any, Optional<Wrapped>> {
