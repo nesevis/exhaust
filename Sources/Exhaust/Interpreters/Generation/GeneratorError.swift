c48fa90ca59884dd0653e36f127f6a8d7b5fa2ce
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2025-08-16 00:12:47 +1000

    Add descriptions to GeneratorError

--- Sources/Exhaust/Interpreters/Generation/GeneratorError.swift
+++ Sources/Exhaust/Interpreters/Generation/GeneratorError.swift
@@ -13,4 +13,20 @@ public enum GeneratorError: LocalizedError {
     case liftFTypeMismatch(expected: String, actual: String)
     case typeMismatch(expected: String, actual: String)
     case sparseValidityCondition
+    
+    var errorDescription: String {
+        switch self {
+        case .couldNotGenerateConcomitantChoiceTree:
+            "Could not generate concomitant choice tree"
+        case .mappedBackwardError(let expected, let actual):
+            "Mapped backward error: expected \(expected), got \(actual)"
+        case .liftFTypeMismatch(let expected, let actual):
+            "LiftF type mismatch: expected \(expected), got \(actual)"
+        case .typeMismatch(let expected, let actual):
+            "Type mismatch: expected \(expected), got \(actual)"
+        case .sparseValidityCondition:
+            "Sparse validity condition"
+        }
+        
+    }
 }
