81eeab9e89fa577532651ee68023227605cec577
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-02-13 15:44:07 +1100

    Milestone: Shrinking Challenge NestedLists complete

--- Tests/ExhaustTests/Challenges/NestedLists.swift
+++ Tests/ExhaustTests/Challenges/NestedLists.swift
@@ -19,8 +19,22 @@ struct NestedListsShrinkingChallenge {
 
      Some libraries, e.g. Hypothesis and jqwik, can shrink this reliably to a single list of 11 elements: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]].
      */
-    @Test("Nested Lists, Full", .disabled("Not implemented"))
-    func nestedListsFull() {
-        // …etc
+    @Test("Nested Lists")
+    func nestedListsFull() async throws {
+        let gen = Gen.arrayOf(Gen.arrayOf(UInt.arbitrary))
+        
+        var count = 0
+        let property: ([[UInt]]) -> Bool = { arr in
+            count += 1
+            return arr.map(\.count).reduce(0, +) <= 10
+        }
+        let iterator = ValueAndChoiceTreeInterpreter(gen, seed: 1337)
+        // Outputs an array of 14 arrays containing 2–20 values each
+        let (_, tree) = Array(iterator.prefix(2)).last!
+        let (_, output) = try #require(try Interpreters.reduce(gen: gen, tree: tree, config: .fast, property: property))
+        // How many times the `property` is called
+        #expect(count == 44)
+        // Shrinks to [[0,…x11]]
+        #expect(output == [Array(repeating: UInt(0), count: 11)])
     }
 }
