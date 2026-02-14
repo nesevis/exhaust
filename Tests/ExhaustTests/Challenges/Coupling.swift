c2c606d6813cb124e376e45fc0e46b5dbe2fff65
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-02-14 23:37:25 +1100

    Implement coupling test

--- Tests/ExhaustTests/Challenges/Coupling.swift
+++ Tests/ExhaustTests/Challenges/Coupling.swift
@@ -18,17 +18,16 @@ struct CouplingShrinkingChallenge {
      The expected smallest falsified sample is [1, 0].
      */
     @Test("Coupling, Full")
+    // We had this, but Minimax destroyed it
     func couplingFull() throws {
-        // A generator that will create an array of length 2...10 with elements corresponding to possible indices
-        let gen = Gen.choose(in: Int(2)...20)
+        let gen = Gen.choose(in: Int(0)...19)
             .bind { n in
-                Gen.arrayOf(Gen.choose(in: 0...n - 1), exactly: UInt64(n))
+                Gen.arrayOf(Gen.choose(in: 0...n), within: 2...20)
             }
         
         // The array cannot contain any 2-cycles, ie where arr[arr[n]] == n
         var count = 0
         let property: ([Int]) -> Bool = { arr in
-            print("Arr count: \(arr.count), indices within bounds \(arr.allSatisfy { arr.indices.contains($0) })")
             count += 1
             // This crashed binarySearchWithGuess.. Wtf?
             return arr.indices.allSatisfy { index in
@@ -41,38 +40,12 @@ struct CouplingShrinkingChallenge {
         }
         
         let iterator = ValueAndChoiceTreeInterpreter(gen, seed: 1337)
-        
         let (value, tree) = Array(iterator.prefix(2)).last!
-        print()
         let (seq, output) = try #require(try Interpreters.reduce(gen: gen, tree: tree, config: .fast, property: property))
         
         // We expect this array to be shortened to only include the two values that cause a cycle
         // And for those two values to be reduced to [0,1] rather than [15, 4]
-        /*
-         └── group
-             ├── choice(signed: 18) 2...20
-             └── sequence(length: 18) 18...18 // This is the issue. It can't shrink the length, so the positions/indices can't be shrunk either
-                 ├── choice(signed: 2) 0...17
-                 ├── choice(signed: 7) 0...17
-                 ├── choice(signed: 7) 0...17
-                 ├── choice(signed: 4) 0...17
-                 ├── choice(signed: 15) 0...17
-                 ├── choice(signed: 16) 0...17
-                 ├── choice(signed: 6) 0...17
-                 ├── choice(signed: 10) 0...17
-                 ├── choice(signed: 10) 0...17
-                 ├── choice(signed: 16) 0...17
-                 ├── choice(signed: 9) 0...17
-                 ├── choice(signed: 3) 0...17
-                 ├── choice(signed: 17) 0...17
-                 ├── choice(signed: 2) 0...17
-                 ├── choice(signed: 8) 0...17
-                 ├── choice(signed: 4) 0...17
-                 ├── choice(signed: 11) 0...17
-         */
         #expect(output.count == 2)
-        print()
-        
-        // Will require a value reduction pass
+        #expect(output == [1, 0])
     }
 }
