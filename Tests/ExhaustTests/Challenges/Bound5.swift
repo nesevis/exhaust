f39d5cde1572a44b13f91950ab7973578fe639e1
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-02-16 15:47:27 +1100

    Create (sort of) property test for reduction

--- Tests/ExhaustTests/Challenges/Bound5.swift
+++ Tests/ExhaustTests/Challenges/Bound5.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Testing
 
+@MainActor
 @Suite("Shrinking Challenge: Bound5")
 struct Bound5ShrinkingChallenge {
     /*
@@ -17,25 +18,54 @@ struct Bound5ShrinkingChallenge {
 
      The interesting thing about this example is the interdependence between separate parts of the sample data. A single list in the tuple will never break the invariant, but you need at least two lists together. This prevents most of trivial shrinking algorithms from getting close to a minimum example, which would look something like ([-32768], [-1], [], [], []).
      */
-    @Test("Bound5, Full")
-    func bound5Full() throws {
+    
+    typealias Bound5 = ([Int16], [Int16], [Int16], [Int16], [Int16])
+    
+    private static let gen: ReflectiveGenerator<Bound5> = {
         let arrGen = Gen.arrayOf(Int16.arbitrary, within: 0...10)
             .filter { $0.isEmpty || $0.dropFirst().reduce($0[0], &+) < 256 }
-        let gen = Gen.zip(arrGen, arrGen, arrGen, arrGen, arrGen)
+        return Gen.zip(arrGen, arrGen, arrGen, arrGen, arrGen)
+    }()
+    
+    private static let property: (Bound5) -> Bool = { (arg) in
+        let (a, b, c, d, e) = arg
+        let arr = a + b + c + d + e
+        return arr.isEmpty == false && arr.dropFirst().reduce(arr[0], &+) < 5 * 256
+    }
+    
+    @Test("Bound5, Single")
+    func bound5Single() throws {
         
-        var count = 0
-        let property: ([Int16], [Int16], [Int16], [Int16], [Int16]) -> Bool = { a, b, c, d, e in
-            count += 1
-            let arr = a + b + c + d + e
-            return arr.isEmpty || arr.dropFirst()
-                .reduce(arr[0], &+) < 5 * 256 // Don't start with 0 when reducing, use the first entry
-        }
-        
-        let iterator = ValueAndChoiceTreeInterpreter(gen, seed: 1337)
+        let iterator = ValueAndChoiceTreeInterpreter(Self.gen, seed: 1337)
         let (value, tree) = Array(iterator.prefix(4)).last!
-        let (seq, output) = try #require(try Interpreters.reduce(gen: gen, tree: tree, config: .fast, property: property))
+        let sequence = ChoiceSequence.flatten(tree)
+        print()
+        let smokeTest = try #require(try Interpreters.materialize(Self.gen, with: tree, using: sequence))
+        #expect(value.0 == smokeTest.0 && value.1 == smokeTest.1 && value.2 == smokeTest.2 && value.3 == smokeTest.3 && value.4 == smokeTest.4)
+        print()
+        let (seq, output) = try #require(try Interpreters.reduce(gen: Self.gen, tree: tree, config: .fast, property: Self.property))
         print("Original value: \(value)")
         print("Output: \(output)")
-        print("Expected output: ([-32768], [-1], [], [], [])")
+        print("Expected output(ish): ([-32768], [-1], [], [], [])")
+    }
+    
+    @Test("Bound5, 50")
+    func bound5Many() throws {
+        let iterator = ValueAndChoiceTreeInterpreter(Self.gen, seed: 1337, maxRuns: 50)
+        
+        var values = [Bound5]()
+        for (value, tree) in iterator where Self.property(value) == false {
+            let (_, output) = try #require(try Interpreters.reduce(gen: Self.gen, tree: tree, config: .fast, property: Self.property))
+            values.append(output)
+        }
+        let list = values.enumerated().sorted(by: { lhs, rhs in
+            let lhsCount = lhs.element.0.count + lhs.element.1.count + lhs.element.2.count + lhs.element.3.count + lhs.element.4.count
+            let rhsCount = rhs.element.0.count + rhs.element.1.count + rhs.element.2.count + rhs.element.3.count + rhs.element.4.count
+            return lhsCount < rhsCount
+        })
+        
+        for (offset, value) in list {
+            print("\(offset + 1): \(String(describing: value).prefix(50))")
+        }
     }
 }
--- Tests/ExhaustTests/Challenges/LargeUnionList.swift
+++ Tests/ExhaustTests/Challenges/LargeUnionList.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Testing
 
+@MainActor
 @Suite("Shrinking Challenge: Large Union List")
 struct LargeUnionListShrinkingChallenge {
     /*
@@ -17,21 +18,78 @@ struct LargeUnionListShrinkingChallenge {
 
      In particular, a shrinker cannot hope to normalise this unless it is able to either split or join elements of the larger list. For example, it would have to be able to transform one of [[0, 1, -1, 2, -2]] and [[0], [1], [-1], [2], [-2]] into the other.
      */
-    @Test("Large Union List, Full")
-    func largeUnionListFull() throws {
+    
+    static let gen: ReflectiveGenerator<[[Int]]> = {
         let arrGen = Gen.arrayOf(Int.arbitrary, within: 1...10)
-        let gen = Gen.arrayOf(arrGen, within: 1...10)
+        return Gen.arrayOf(arrGen, within: 1...10)
+    }()
+    
+    static let property: ([[Int]]) -> Bool = { arr in
+        Set(arr.flatMap(\.self)).count <= 4
+    }
+    
+    @Test("Large Union List, Single")
+    func largeUnionListFull() throws {
+        let iterator = ValueAndChoiceTreeInterpreter(Self.gen, seed: 1337)
+        let (value, tree) = Array(iterator.prefix(4)).last! // 23 values
+        let (sequence, output) = try #require(try Interpreters.reduce(gen: Self.gen, tree: tree, config: .fast, property: Self.property))
+        #expect(output.flatMap(\.self) == [-1, -0, 1, 2, 3])
+    }
+    
+    @Test("Large Union List, Pathological single")
+    func largeUnionListPathological() throws {
+        let value = [
+            [-40236158320423685, 56599776734305647, -110764793782677473],
+            [-173728398250472629],
+            [-92071603954950552]
+        ]
+        let tree = try #require(try Interpreters.reflect(Self.gen, with: value))
+        print()
+        let (_, output) = try #require(try Interpreters.reduce(gen: Self.gen, tree: tree, config: .fast, property: Self.property))
+        print(output)
+        #expect(output.flatMap(\.self) == [-3, -2, -1, 0, 1])
+    }
+    
+    @Test("Large Union List, Pathological single 2")
+    func largeUnionListPathological2() throws {
+        let value = [[-140165314328449, 79003739596584, -102880757906973, 59059092428908, 118937662940338, 110119751840770, 100385325416037, -118755354749898, 80572987607965, 76424960810766], [-28023762322669, 11702849741616, -132960251314433, 123682815435579, -10343261662018, -4700527354204, 10032215627723, -63802894155092, -103439992132983], [-31190610291605, -125312221647467, -67770770878048, 74921319749072, -34565939758906, -48688160340287, 18293331003577, 67560200516186], [-77447398498565, -126080081874646, -63017712975195, 86926291646097], [-89717625244173, -10050986803917, 10364103939241, -93995600961861, 31194551855121, -132988363192036, -96151068047749], [-22614648784524, -18194426629298, 123098101697801, 73283960328215, -24300919081696, -18576827148737, -71742940518794], [-6813118022644, 57217985601415, -6180874521902, -136303770089928]]
+        let tree = try #require(try Interpreters.reflect(Self.gen, with: value))
+        print()
+        let (_, output) = try #require(try Interpreters.reduce(gen: Self.gen, tree: tree, config: .fast, property: Self.property))
+        print(output)
+        #expect(output.flatMap(\.self) == [-1, 0, 1, 2, 3])
+    }
+    
+    @Test("Large Union List, 50")
+    func largeUnionListBatch() throws {
+        let iterator = ValueAndChoiceTreeInterpreter(Self.gen, seed: 1337, maxRuns: 100)
         
-        var count = 0
-        let property: ([[Int]]) -> Bool = { arr in
-            count += 1
-            return Set(arr.flatMap(\.self)).count <= 4
+        var outputs = [[[Int]]]()
+        for (value, tree) in iterator where Self.property(value) == false && outputs.count <= 50 {
+            let (seq, output) = try #require(try Interpreters.reduce(gen: Self.gen, tree: tree, config: .fast, property: Self.property))
+            outputs.append(output)
+            // This fails due to the validRange of the generator's size interacting with the randomly generated value causing certain passes from working as intended.
+//            if output.map(\.count).reduce(0, +) > 5 {
+//                print("value: \(value)")
+//                print("output: \(output)")
+//            }
+//            #expect(output.flatMap(\.self) == [-2, -1, 0, 1, 2])
         }
         
-        let iterator = ValueAndChoiceTreeInterpreter(gen, seed: 1337)
-        let (value, tree) = Array(iterator.prefix(3)).last! // 23 values
-        let (sequence, output) = try #require(try Interpreters.reduce(gen: gen, tree: tree, config: .fast, property: property))
-        #expect(count == 219)
-        #expect(output.flatMap(\.self) == [-2, -1, 0, 1, 2])
+        for (index, output) in outputs.enumerated() {
+            print("\(index + 1): \(output)")
+            // Expect there to be one nested array
+            #expect(output.count == 1)
+            // Expect there to be five entries in this array
+            let array = try #require(output.first)
+            #expect(array.count == 5)
+            
+            // Expect the values to increase by one
+            var steps = Set<Int>()
+            for (index, value) in array.enumerated().dropFirst() {
+                steps.insert(value - array[index - 1])
+            }
+            #expect(steps == [1])
+        }
     }
 }
