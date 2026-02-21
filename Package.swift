585e94738cf7b7d2d879bb88d9d970c02d165dc0
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-02-22 00:02:07 +1100

    Add Doc-C package

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
         .package(url: "https://github.com/google/swift-benchmark", from: "0.1.2"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.59.1"),
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.1")
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.1"),
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
