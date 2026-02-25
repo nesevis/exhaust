ee743b3ce44ce8aae4cab3a23a8d48f1faa6e0e9
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-02-25 17:49:26 +1100

    Remove half finished CGS work

--- Sources/Exhaust/Interpreters/Types/ChoiceMetadata.swift
+++ Sources/Exhaust/Interpreters/Types/ChoiceMetadata.swift
@@ -8,12 +8,4 @@
 public struct ChoiceMetadata: Hashable, Equatable, Sendable {
     /// `Character` has discontiguous ranges, and `RangeSet` isn't available until the very newest releases
     public let validRanges: [ClosedRange<UInt64>]
-
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(validRanges)
-    }
-
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.hashValue == rhs.hashValue
-    }
 }
