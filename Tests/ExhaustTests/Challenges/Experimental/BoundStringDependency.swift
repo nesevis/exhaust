8a76d66896143d7a5ef15d4d5fd753fcb2968e91
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-03-13 09:20:28 +1100

    Add BoundStringDependency test

---
+++ Tests/ExhaustTests/Challenges/Experimental/BoundStringDependency.swift
@@ -0,0 +1,30 @@
+//
+//  DependentString.swift
+//  Exhaust
+//
+//  Created by Chris Kolbu on 13/3/2026.
+//
+
+import Exhaust
+import Testing
+
+@Suite("Experimental Challenge: Dependent String")
+struct DependentStringChallenge {
+    /*
+     The generator returned from `bound` is irrelevant to the property, which only tests for length
+     */
+    @Test("Bound string dependency")
+    func testBoundStringDependency() throws {
+        ExhaustLog.setConfiguration(.init(isEnabled: true, minimumLevel: .info, categoryMinimumLevels: [.reducer: .debug], format: .human))
+        let gen = #gen(.int(in: 0 ... 10)).bound(
+            forward: { .string(length: UInt64($0) ... UInt64($0)) },
+            backward: \.count
+        )
+
+        let output = #exhaust(gen, .suppressIssueReporting) { value in
+            !(4...5 ~= value.count)
+        }
+
+        #expect(output == "    ")
+    }
+}
