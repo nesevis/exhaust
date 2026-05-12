55a3d68bd883d34f3f8c55904ebbead43f7e11c9
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-05-12 23:29:37 +1000

    Move Interpreters definition to its own file

---
+++ Sources/ExhaustCore/Interpreters/Interpreters.swift
@@ -0,0 +1,7 @@
+//
+//  Interpreters.swift
+//  Exhaust
+//
+
+/// Namespace for generator interpreter entry points covering generation, reflection, replay, and reduction.
+package enum Interpreters {}
