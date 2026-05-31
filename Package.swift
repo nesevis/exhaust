2723e5248b41638cfc68ea143f7899c39f1e31a7
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-06-01 00:28:23 +1000

    Disable macro tests
    CLI and Xcode do not agree on what the output should be

--- Package.swift
+++ Package.swift
@@ -86,14 +86,14 @@ let package = Package(
             dependencies: ["Exhaust", "ExhaustCore", "ExhaustTestSupport"],
             plugins: swiftLintPlugins
         ),
-        .testTarget(
-            name: "ExhaustMacrosTests",
-            dependencies: [
-                "Exhaust",
-                "ExhaustMacros",
-                .product(name: "MacroTesting", package: "swift-macro-testing"),
-            ]
-        ),
+//        .testTarget(
+//            name: "ExhaustMacrosTests",
+//            dependencies: [
+//                "Exhaust",
+//                "ExhaustMacros",
+//                .product(name: "MacroTesting", package: "swift-macro-testing"),
+//            ]
+//        ),
         .executableTarget(
             name: "ExhaustBenchmarks",
             dependencies: [
